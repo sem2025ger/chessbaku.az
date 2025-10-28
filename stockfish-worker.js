0c7cf4995cb55b1cd254dbb31503bfc702200314
sem2025ger <semov.20207@gmail.com>
2025-10-28 02:05:57 +0100

    Refactor Stockfish worker to use WASM version

--- stockfish-worker.js
+++ stockfish-worker.js
@@ -1,59 +1,87 @@
 // stockfish-worker.js
 
-// Загружаем движок Stockfish напрямую из CDN
-importScripts('https://unpkg.com/stockfish@16.1.0/src/stockfish.js');
+// Загружаем движок Stockfish из CDN
+importScripts('https://cdn.jsdelivr.net/npm/stockfish.wasm@0.11.0/stockfish.js');
 
 let engine = null;
 let ready = false;
 
-function send(cmd) {
-  if (engine) engine.postMessage(cmd);
+// Инициализация Stockfish WASM
+Stockfish().then(function(sf) {
+  engine = sf;
+  
+  // Обработчик сообщений от движка
+  engine.addMessageListener(function(message) {
+    if (message === 'uciok') {
+      ready = true;
+      self.postMessage({ type: 'ready' });
+    } else if (typeof message === 'string') {
+      if (message.startsWith('bestmove')) {
+        const parts = message.split(' ');
+        self.postMessage({ type: 'bestmove', bestmove: parts[1] });
+      } else if (message.startsWith('info')) {
+        const info = parseInfo(message);
+        if (Object.keys(info).length > 0) {
+          self.postMessage({ type: 'info', info: info });
+        }
+      }
+    }
+  });
+  
+  // Отправляем команду uci для инициализации
+  engine.postMessage('uci');
+});
+
+// Функция парсинга информации от движка
+function parseInfo(line) {
+  const info = {};
+  
+  // Глубина поиска
+  const depthMatch = line.match(/depth (\d+)/);
+  if (depthMatch) info.depth = depthMatch[1];
+  
+  // Количество узлов
+  const nodesMatch = line.match(/nodes (\d+)/);
+  if (nodesMatch) info.nodes = nodesMatch[1];
+  
+  // Скорость (узлов в секунду)
+  const npsMatch = line.match(/nps (\d+)/);
+  if (npsMatch) info.nps = npsMatch[1];
+  
+  // Время
+  const timeMatch = line.match(/time (\d+)/);
+  if (timeMatch) info.time = timeMatch[1];
+  
+  // Оценка позиции
+  const scoreMatch = line.match(/score (cp|mate) (-?\d+)/);
+  if (scoreMatch) {
+    info.score = scoreMatch[1] + ' ' + scoreMatch[2];
+  }
+  
+  // Главная вариация (PV)
+  const pvMatch = line.match(/pv (.+)/);
+  if (pvMatch) info.pv = pvMatch[1];
+  
+  return info;
 }
 
-engine = typeof STOCKFISH === "function" ? STOCKFISH() : STOCKFISH;
-engine.onmessage = function (event) {
-  const line = event.data || event;
-  if (line === 'uciok') {
-    ready = true;
-    postMessage({ type: 'ready' });
-  } else if (typeof line === 'string') {
-    if (line.startsWith('bestmove')) {
-      const parts = line.split(' ');
-      postMessage({ type: 'bestmove', bestmove: parts[1] });
-    } else if (line.startsWith('info')) {
-      const info = {};
-      const matchers = {
-        depth: /depth (\d+)/,
-        nodes: /nodes (\d+)/,
-        nps: /nps (\d+)/,
-        time: /time (\d+)/,
-        score: /score (cp|mate) (-?\d+)/,
-        pv: /pv (.+)/,
-      };
-      for (const key in matchers) {
-        const match = line.match(matchers[key]);
-        if (match) info[key] = match[1] || match[0];
-      }
-      postMessage({ type: 'info', info });
-    }
-  }
-};
-
-send('uci');
-
-onmessage = function (event) {
-  if (!ready) return;
+// Обработчик сообщений от главного потока
+self.onmessage = function(event) {
+  if (!engine || !ready) return;
+  
   const { type, fen } = event.data;
+  
   if (type === 'init') {
-    send('setoption name Skill Level value 20');
-    send('isready');
+    engine.postMessage('setoption name Skill Level value 20');
+    engine.postMessage('setoption name MultiPV value 1');
+    engine.postMessage('isready');
   } else if (type === 'analyze') {
-    send('stop');
-    send(`position fen ${fen}`);
-    send('go infinite');
+    engine.postMessage('stop');
+    engine.postMessage('position fen ' + fen);
+    engine.postMessage('go infinite');
   } else if (type === 'getmove') {
-    send('stop');
-    send(`position fen ${fen}`);
-    send('go movetime 1500');
+    engine.postMessage('stop');
+    engine.postMessage('position fen ' + fen);
+    engine.postMessage('go movetime 1500');
   }
 };
